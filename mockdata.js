c9157a2f1eb254ab3c67e46bd8271138327827c6
unelon <rlillevang@gmail.com>
2023-07-12 12:26:17 +0200

    first

---
+++ mockdata.js
@@ -0,0 +1,143 @@
+const mockData = [
+	{
+		"title": "Chuck Norris' Kickin' Chicken Tacos",
+		"id": "001",
+		"short_description": "Tacos that pack a punch, just like Chuck Norris",
+		"link": "/kickin-tacos.html",
+		"image": "https://images.pexels.com/photos/2092507/pexels-photo-2092507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		"ingredients": [
+			"1 lb. chicken breast, diced",
+			"1 tbsp. olive oil",
+			"1 tsp. chili powder",
+			"1/2 tsp. cumin",
+			"1/2 tsp. garlic powder",
+			"1/2 tsp. salt",
+			"1/4 tsp. black pepper",
+			"8 small tortillas",
+			"1 cup shredded lettuce",
+			"1 cup diced tomatoes",
+			"1/2 cup shredded cheddar cheese",
+			"1/4 cup chopped fresh cilantro",
+			"Salsa and sour cream for serving"
+		],
+		"instructions": [
+			"In a large skillet, heat olive oil over medium-high heat.",
+			"Add diced chicken to the skillet and season with chili powder, cumin, garlic powder, salt, and black pepper.",
+			"Cook the chicken until it is cooked through and slightly browned, about 5-7 minutes.",
+			"Warm the tortillas in a separate skillet or in the oven.",
+			"Assemble the tacos by placing a spoonful of the cooked chicken onto each tortilla.",
+			"Top with shredded lettuce, diced tomatoes, shredded cheddar cheese, and chopped cilantro.",
+			"Serve the tacos with salsa and sour cream on the side.",
+			"Enjoy these kickin' chicken tacos that are sure to pack a punch, just like Chuck Norris!"
+		]
+	},
+	{
+		"title": "The Roundhouse Pasta",
+		"id": "002",
+		"short_description": "A pasta dish that will roundhouse kick your taste buds",
+		"link": "/roundhouse-pasta.html",
+		"image": "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		"ingredients": [
+			"8 oz. spaghetti",
+			"2 tbsp. olive oil",
+			"4 cloves garlic, minced",
+			"1/2 tsp. red pepper flakes",
+			"1 can (14 oz.) diced tomatoes",
+			"1/4 cup chopped fresh basil",
+			"1/4 cup grated Parmesan cheese",
+			"Salt and black pepper to taste"
+		],
+		"instructions": [
+			"Cook the spaghetti according to package instructions until al dente. Drain and set aside.",
+			"In a large skillet, heat olive oil over medium heat.",
+			"Add minced garlic and red pepper flakes to the skillet. Cook until the garlic is golden and fragrant, about 1-2 minutes.",
+			"Add the diced tomatoes (with juice) to the skillet. Simmer for 5 minutes, stirring occasionally.",
+			"Season the tomato sauce with salt and black pepper to taste.",
+			"Add the cooked spaghetti to the skillet and toss to coat the pasta with the sauce.",
+			"Remove from heat and garnish with chopped fresh basil and grated Parmesan cheese.",
+			"Serve the Roundhouse Pasta with a side of sliced bread and prepare to have your taste buds roundhouse kicked by the flavors!",
+			"Enjoy!"
+		]
+	},
+	{
+		"title": "Chuck Norris' Ultimate Steak",
+		"id": "003",
+		"short_description": "The steak that will make you feel invincible, just like Chuck Norris",
+		"link": "/ultimate-steak.html",
+		"image": "https://images.pexels.com/photos/3535383/pexels-photo-3535383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		"ingredients": [
+			"2 ribeye steaks",
+			"2 tbsp. olive oil",
+			"2 cloves garlic, minced",
+			"1 tsp. dried rosemary",
+			"1 tsp. dried thyme",
+			"Salt and black pepper to taste",
+			"1/4 cup butter",
+			"1 sprig fresh rosemary, for garnish"
+		],
+		"instructions": [
+			"Preheat the grill to high heat.",
+			"Rub the steaks with olive oil, minced garlic, dried rosemary, dried thyme, salt, and black pepper.",
+			"Place the steaks on the grill and cook for 4-5 minutes per side for medium-rare, or adjust the cooking time to your desired level of doneness.",
+			"While the steaks are cooking, melt the butter in a small saucepan over low heat.",
+			"Once the steaks are cooked to your liking, remove them from the grill and let them rest for a few minutes.",
+			"Drizzle the melted butter over the steaks and garnish with a sprig of fresh rosemary.",
+			"Serve the Ultimate Steak with your favorite sides, and savor the taste of a truly invincible meal, just like Chuck Norris!",
+			"Enjoy!"
+		]
+	},
+	{
+		"title": "Chuck Norris' Dynamite Nachos",
+		"id": "004",
+		"short_description": "Explosively delicious nachos that will blow you away",
+		"link": "/dynamite-nachos.html",
+		"image": "https://media.istockphoto.com/id/460140985/photo/dynamite.jpg?b=1&s=612x612&w=0&k=20&c=nqfAV2uttTKMbFvQM-JX1JG0N8Qv1xb2-ZBbBnrIGIA=",
+		"ingredients": [
+			"1 bag tortilla chips",
+			"1 lb. ground beef",
+			"1 packet taco seasoning",
+			"1 cup shredded cheddar cheese",
+			"1 cup diced tomatoes",
+			"1/2 cup diced red onion",
+			"1/4 cup sliced jalapeños",
+			"1/4 cup sliced black olives",
+			"1/4 cup chopped fresh cilantro",
+			"Sour cream and guacamole for serving"
+		],
+		"instructions": [
+			"Preheat the oven to 350°F (175°C).",
+			"In a skillet, cook the ground beef over medium heat until browned. Drain any excess fat.",
+			"Add the taco seasoning and water according to the packet instructions. Stir and cook until well combined.",
+			"Arrange the tortilla chips in a single layer on a baking sheet.",
+			"Sprinkle the cooked ground beef evenly over the tortilla chips.",
+			"Top with shredded cheddar cheese, diced tomatoes, diced red onion, sliced jalapeños, and sliced black olives.",
+			"Bake in the preheated oven for 10-12 minutes, or until the cheese has melted and the nachos are heated through.",
+			"Remove from the oven and garnish with chopped fresh cilantro.",
+			"Serve the Dynamite Nachos with sour cream and guacamole for dipping, and get ready for an explosively delicious snack!",
+			"Enjoy!"
+		]
+	},
+	{
+		"title": "Chuck Norris' Power-Packed Smoothie",
+		"id": "005",
+		"short_description": "A nutritious and energizing smoothie to unleash your inner strength",
+		"link": "/power-packed-smoothie.html",
+		"image": "https://images.pexels.com/photos/1346347/pexels-photo-1346347.jpeg?auto=compress&cs=tinysrgb&w=1600",
+		"ingredients": [
+			"1 ripe banana",
+			"1 cup spinach leaves",
+			"1/2 cup Greek yogurt",
+			"1/4 cup almond butter",
+			"1 tbsp. honey",
+			"1 cup almond milk",
+			"1/2 cup ice cubes"
+		],
+		"instructions": [
+			"In a blender, combine the ripe banana, spinach leaves, Greek yogurt, almond butter, honey, almond milk, and ice cubes.",
+			"Blend on high speed until smooth and creamy, ensuring all ingredients are well combined.",
+			"Pour the Power-Packed Smoothie into a glass and serve immediately.",
+			"Sip on this nutritious and energizing smoothie to unleash your inner strength, just like Chuck Norris!",
+			"Enjoy!"
+		]
+	}
+];
